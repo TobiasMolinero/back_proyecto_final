6df32750b05ddd400c53adc431a44ad1cca5cbce
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-06-14 17:39:11 -0300

    se cambio el middleware 'comprobarGastoEliminado' a 'comprobarCatGastoEliminado'

--- src/middlewares/gasto.middleware.js
+++ src/middlewares/gasto.middleware.js
@@ -1,18 +1,18 @@
 import { pool } from "../db.js";
 
-export const comprobarGastoEliminado = (req, res, next) => {
-    const {detalle} = req.body
+export const comprobarCatGastoEliminado = (req, res, next) => {
+    const {descripcion} = req.body
 
-    pool.query(`SELECT id_gasto, detalle FROM gasto
-                WHERE detalle = '${detalle}' AND estado_registro = 0
+    pool.query(`SELECT descripcion FROM categoria_gasto
+                WHERE descripcion = '${descripcion}' AND estado_registro = 0
     `, (error, results) => {
         if(error){
             res.status(500).json({
                 message: 'Ocurrio un error en el servidor. Vuelva a intentarlo más tarde.'
             })
         } else if(results.length !== 0){
-            pool.query(`UPDATE gasto SET estado_registro = 1
-                        WHERE detalle = '${detalle}' AND estado_registro = 0    
+            pool.query(`UPDATE categoria_gasto SET estado_registro = 1
+                        WHERE descripcion = '${descripcion}' AND estado_registro = 0    
             `, (error) => {
                 if(error){
                     res.status(500).json({
--- src/routes/gasto.route.js
+++ src/routes/gasto.route.js
@@ -1,18 +1,18 @@
 import { Router } from 'express'
 import { all, allCategorias, bajaCategoria, borrar, crear, crearCategoria, editar, editarCategoria, one } from '../controllers/gasto.controller.js'
-import { comprobarGastoEliminado } from '../middlewares/gasto.middleware.js'
+import { comprobarCatGastoEliminado } from '../middlewares/gasto.middleware.js'
 
 const router = Router()
 
 router.get('/gastos/all', all)
 router.get('/gastos/one/:id', one)
-router.post('/gastos/create', comprobarGastoEliminado, crear)
+router.post('/gastos/create', crear)
 router.put('/gastos/edit/:id', editar)
 router.delete('/gastos/delete/:id', borrar)
 
 //Categoria gasto
 router.get('/cat-gastos/all', allCategorias)
-router.post('/cat-gastos/create', crearCategoria)
+router.post('/cat-gastos/create', comprobarCatGastoEliminado, crearCategoria)
 router.put('/cat-gastos/edit/:id', editarCategoria)
 router.put('/cat-gastos/delete/:id', bajaCategoria)
 
