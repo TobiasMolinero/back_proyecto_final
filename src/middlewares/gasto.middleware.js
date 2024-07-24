52c3ca79183e06f69dd8bfd884d10993f4213e4b
Tobias Molinero <tobiasmolinero98@gmail.com>
2024-07-24 20:32:50 -0300

    cambios en middleware de gastos

--- src/middlewares/gasto.middleware.js
+++ src/middlewares/gasto.middleware.js
@@ -3,8 +3,8 @@ import { pool } from "../db.js";
 export const comprobarCatGastoEliminado = (req, res, next) => {
     const {descripcion} = req.body
 
-    pool.query(`SELECT descripcion FROM categoria_gasto
-                WHERE descripcion = '${descripcion}' AND estado_registro = 0
+    pool.query(`SELECT categoria FROM categoria_gasto
+                WHERE categoria = '${descripcion}' AND estado_registro = 0
     `, (error, results) => {
         if(error){
             res.status(500).json({
@@ -12,7 +12,7 @@ export const comprobarCatGastoEliminado = (req, res, next) => {
             })
         } else if(results.length !== 0){
             pool.query(`UPDATE categoria_gasto SET estado_registro = 1
-                        WHERE descripcion = '${descripcion}' AND estado_registro = 0    
+                        WHERE categoria = '${descripcion}' AND estado_registro = 0    
             `, (error) => {
                 if(error){
                     res.status(500).json({
